4fcc31374f5426000ed98b513568a2a9429f2153
ilkinsufi <ilkinsufi@gmail.com>
2024-11-11 12:12:43 +0400

    calendar remove

--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,6 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import { ChartContainer, ChartTooltipContent } from "@/components/ui/charts";
-
-import { Bar, BarChart } from "recharts";
 
 import {
   AlertDialog,
@@ -33,18 +30,8 @@ import {
 } from "@/components/ui/alert-dialog";
 import React from "react";
 import { Calendar } from "@/components/ui/calendar";
-import { type ChartConfig } from "@/components/ui/chart"
-
 
 export default function Home() {
-  const chartData = [
-    { month: "January", desktop: 186, mobile: 80 },
-    { month: "February", desktop: 305, mobile: 200 },
-    { month: "March", desktop: 237, mobile: 120 },
-    { month: "April", desktop: 73, mobile: 190 },
-    { month: "May", desktop: 209, mobile: 130 },
-    { month: "June", desktop: 214, mobile: 140 },
-  ];
   const [date, setDate] = React.useState<Date | undefined>(new Date());
 
   return (
@@ -125,13 +112,6 @@ export default function Home() {
           onSelect={setDate}
           className="rounded-md border"
         />
-        <hr className="my-6" />
-        <ChartContainer>
-          <BarChart data={data}>
-            <Bar dataKey="value" />
-            <ChartTooltip content={<ChartTooltipContent />} />
-          </BarChart>
-        </ChartContainer>
         <hr className="my-6" />
       </div>
     </main>
